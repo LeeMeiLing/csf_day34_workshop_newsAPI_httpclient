728fc9b45999870771ed43c619aaba4bd40c9d48
LeeMeiLing <leem0060@outlook.com>
2023-04-20 15:19:38 +0800

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Article, COUNTRIES, NEWS_API_KEY, NEWS_URL, SOURCE_URL, Source } from './models';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, Subscription, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'day34-workshop-newsAPI';
+export class AppComponent implements OnInit{
+
+  form!:FormGroup
+  countries = COUNTRIES
+  sources:Source[] = []
+  selectedCountry!:string
+  selectedSourceId!:string
+  source$!:Subscription
+  news$!:Subscription
+  articles:Article[]=[]
+
+  constructor(private fb:FormBuilder,private http:HttpClient){}
+
+  ngOnInit(): void {
+    this.form = this.createForm()
+  }
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      country:this.fb.control<string>('',[Validators.required]),
+      source:this.fb.control<string>('',[Validators.required])
+    })
+  }
+  
+  getSource(){
+    // read the selected country code
+    this.selectedCountry = this.form.value['country']
+
+    // create query params
+    const params = new HttpParams().set('apiKey',NEWS_API_KEY).set('country',this.selectedCountry)
+
+    // returns an observable
+    this.source$ = this.http.get<any>(SOURCE_URL,{params})
+      .subscribe({
+        next: v => {
+          console.info('.....NEXT')
+          this.sources = v['sources'] as Source[]
+          console.info("sources[]: ",this.sources)
+        },
+        error: err => {
+          console.error('ERROR: ', err)
+        },
+        complete() {
+            console.warn('......COMPLETE: ')
+        },
+      })
+
+  }
+
+  getNews(){
+    this.selectedSourceId = this.form.value['source']
+
+    //https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=94b4d80e48ed48cdaa9a7fd9ce350654
+
+    // create query params
+    const params = new HttpParams().set('sources',this.selectedSourceId).set('apiKey',NEWS_API_KEY)
+
+    // returns an observable
+    this.news$ = this.http.get<any>(NEWS_URL,{params})
+      .subscribe({
+        next: v => {
+          console.info('.....NEXT')
+          this.articles = v['articles'] as Article[]
+        },
+        error: err => {
+          console.error('ERROR: ', err)
+        },
+        complete() {
+            console.warn('......COMPLETE: ')
+        },
+      })
+  }
+  
+
 }
---
+++ src/app/models.ts
@@ -0,0 +1,39 @@
+export const COUNTRIES = [ 'ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn',
+	'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in',
+	'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl',
+	'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us',
+	've', 'za' ]
+
+export const NEWS_URL = 'https://newsapi.org/v2/top-headlines'
+
+export const SOURCE_URL = 'https://newsapi.org/v2/top-headlines/sources'
+
+export const NEWS_API_KEY = '94b4d80e48ed48cdaa9a7fd9ce350654'
+
+export interface Source{
+	
+	id: string,
+	name: string,
+	description: string,
+	url: string,
+	category: string,
+	language: string,
+	country: string
+	
+}
+
+export interface Source{
+	id:string,
+	name: string
+}
+
+export interface Article{
+
+		source: Source,
+		author: string,
+		title: string,
+		description: string,
+		urlToImage: string,
+		content: string
+
+}
